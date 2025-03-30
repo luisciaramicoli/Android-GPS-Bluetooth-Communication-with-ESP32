a8187573c9a3ccb6982f60024cd902fe44d204fb
luisciaramicoli <luisgustavobonfim996@gmail.com>
2025-03-30 03:52:23 -0300

    funciona

--- app/src/main/java/com/andreseptian/realtimegpsdata/BluetoothGPSService.kt
+++ app/src/main/java/com/andreseptian/realtimegpsdata/BluetoothGPSService.kt
@@ -1,5 +1,8 @@
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -7,19 +10,40 @@ import androidx.core.app.NotificationManagerCompat
 
 class BluetoothGPSService : Service() {
 
+    companion object {
+        private const val CHANNEL_ID = "GPS_CHANNEL"
+        private const val NOTIFICATION_ID = 1
+    }
+
     override fun onCreate() {
         super.onCreate()
         Log.d("BluetoothGPSService", "Serviço iniciado")
-        
+
+        // Criar o canal de notificação para Android 8.0 ou superior
+        createNotificationChannel()
+
         // Criar uma notificação para rodar em primeiro plano
-        val notification = NotificationCompat.Builder(this, "GPS_CHANNEL")
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Serviço de GPS e Bluetooth")
             .setContentText("Monitorando a localização e conexão Bluetooth")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(android.R.drawable.ic_menu_mylocation) // Ícone padrão do Android
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .build()
 
-        startForeground(1, notification)
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Serviço de GPS e Bluetooth"
+            val descriptionText = "Monitoramento contínuo da localização e do Bluetooth"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
